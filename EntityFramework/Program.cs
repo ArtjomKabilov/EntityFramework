d985fccd51a1de0abab44ff9b9c63cf31ef61749
opilane <opilane@HP-CZC6297BB4>
2022-09-16 11:30:06 +0300

    Add project files.

---
+++ EntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using EntityFramework;
+
+using (helloappContext db = new helloappContext())
+{
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("Список объектов:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name},{u.Surname},{u.LivePlace},{u.email},{u.Age}");
+    }
+}
