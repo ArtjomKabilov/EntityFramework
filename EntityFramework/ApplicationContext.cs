e4785070a46b99c9e9d68277cb87d0b66ee1dedd
opilane <opilane@HP-CZC6297BB4>
2022-09-21 09:31:14 +0300

    Uff

--- EntityFramework/ApplicationContext.cs
+++ EntityFramework/ApplicationContext.cs
@@ -11,11 +11,10 @@ namespace EntityFramework
     public class ApplicationContext : DbContext
     {
         public DbSet<User> Users { get; set; } = null!;
-        public ApplicationContext() => Database.EnsureCreated();
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+                : base(options)
         {
-            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+            Database.EnsureCreated();
         }
     }
 }
